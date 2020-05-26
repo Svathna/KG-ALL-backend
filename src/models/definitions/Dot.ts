63d8e4111d37ade9e7aa5d78cf3f0c50a873fc58
Svathna <s.s.vathna@gmail.com>
2020-05-26 21:17:59 +0700

    change on vat

---
+++ src/models/definitions/Dot.ts
@@ -0,0 +1,35 @@
+import { Typegoose, prop, Ref } from 'typegoose';
+
+class Dot extends Typegoose {
+  @prop({ required: true })
+  dotNumber: number;
+
+  @prop({ required: true })
+  notedDate: Date;
+
+  @prop({ required: true })
+  dotBranch: string;
+
+  @prop({ required: true })
+  address: string;
+
+  @prop({ required: true })
+  bankName: string;
+
+  @prop({ required: true })
+  bankAccountName: string;
+
+  @prop({ required: true })
+  bankAccountNumber: number;
+
+  @prop({ required: true })
+  taxCardNumber: string;
+
+  @prop({ required: true })
+  phoneNumber: string;
+
+  @prop({ required: true, default: false })
+  deleted: boolean;
+}
+
+export default Dot;
